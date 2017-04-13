a526b9554ba8ba47d31befe039bc303b17f52703
Mat Mariani <someone.mariani@gmail.com>
2017-04-13 18:26:32 -0400

    Updated texture loading

--- src/textures.cc
+++ src/textures.cc
@@ -1,29 +1,54 @@
 #include "textures.h"
 #include "libraries/soil/SOIL.h"
 
-GLuint Textures::load(const std::string& name, const std::string& path) {
-	GLuint texture;
-	glGenTextures(1, &texture);
-	glBindTexture(GL_TEXTURE_2D, texture);
+void Textures::destroy(const std::string& name) {
+	auto itr = cache.find(name);
+	if (itr != cache.end()) {
+		destroy(&itr->second);
+		cache.erase(itr);
+	}
+}
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+void Textures::destroy(Texture* texture) {
+	if (texture == nullptr) {
+		return;
+	}
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glDeleteTextures (1, &texture->handle);
+}
 
+Texture* Textures::get(const std::string& name) {
+	auto itr = cache.find(name);
+	return (itr != cache.end()) ? (&itr->second) : &(cache[name] = load(name));
+}
+
+
+Texture Textures::load(const std::string& name) {
 	int w, h;
-	auto image = SOIL_load_image(path.c_str(), &w, &h, 0, SOIL_LOAD_RGB);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
-	glGenerateMipmap(GL_TEXTURE_2D);
-	glBindTexture(GL_TEXTURE_2D, 0);
+	auto image = SOIL_load_image(("assets/" + name).c_str(), &w, &h, 0, SOIL_LOAD_RGB);
 
-	cache[name] = texture;
+	GLuint textureId;
+	glGenTextures (1, &textureId);
+
+	Texture texture;
+	texture.handle = textureId;
+	texture.width = w;
+	texture.height = h;
+
+	glEnable (GL_TEXTURE_2D);
+
+	glBindTexture (GL_TEXTURE_2D, textureId);
+
+	glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glPixelStorei (GL_UNPACK_ALIGNMENT, 4);
+	glTexImage2D (GL_TEXTURE_2D, 0, GL_RGBA, texture.width, texture.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, (unsigned char*)image);
+
+	glDisable (GL_TEXTURE_2D);
+
 	SOIL_free_image_data(image);
 
 	return texture;
-}
-
-GLuint Textures::get(const std::string& name) {
-	return cache[name];
 }
--- src/textures.h
+++ src/textures.h
@@ -7,14 +7,22 @@
 // gl3w
 #include "opengl/gl3w.h"
 
+typedef struct {
+	GLuint handle;
+	int width, height;
+} Texture;
+
 class Textures {
 public:
 	Textures() = default;
 	~Textures() = default;
 
-	GLuint load(const std::string& name, const std::string& path);
-	GLuint get(const std::string& name);
+	void destroy(const std::string& name);
+	void destroy(Texture* texture);
+
+	Texture* get(const std::string& name);
 
 private:
-	std::unordered_map<std::string, GLuint> cache;
+	Texture load(const std::string& name);
+	std::unordered_map<std::string, Texture> cache;
 };
